01229c13cd25721493bf70d78ee99e08cb16077f
OsirisJr16 <mintsanavalonajunior@example.com>
2025-05-15 18:31:50 +0300

    FEAT/Mal_Voice

--- app/main.py
+++ app/main.py
@@ -1,25 +1,29 @@
 from fastapi import FastAPI
-from gtts import gTTS
 from fastapi.responses import FileResponse
 from starlette.middleware.cors import CORSMiddleware
+import edge_tts
+import uuid
+import os
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],  
-    allow_headers=["*"],  
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+
+
+MASCULINE_VOICE = "fr-FR-HenriNeural"
 
 @app.post("/generate_speech/")
 async def generate_speech(text: str):
-   
-    tts = gTTS(text, lang='fr')
-    output_file = "output.mp3"
-
-    tts.save(output_file)
     
+    filename = f"{uuid.uuid4().hex}.mp3"
 
-    return FileResponse(output_file, media_type="audio/mp3", filename="output.mp3")
+    communicate = edge_tts.Communicate(text, voice=MASCULINE_VOICE)
+    await communicate.save(filename)
+
+    return FileResponse(filename, media_type="audio/mp3", filename="speech.mp3")
